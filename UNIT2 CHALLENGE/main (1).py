f0bdd5ac42de0a7975600685567f4f8bd73eb6b0
sivakanishka19 <144413948+sivakanishka19@users.noreply.github.com>
2023-09-13 14:57:41 +0530

    Add files via upload

---
+++ UNIT2 CHALLENGE/main (1).py
@@ -0,0 +1,13 @@
+class Player:
+  def play(self):
+    print("The player is playing cricket")
+class Batsman:
+  def play(self):
+    print("The Batsman is Batting")
+class Bowler:
+  def play(self):
+    print("The Bowler is Bowling")
+batsman=Batsman()
+bowler=Bowler()
+batsman.play()
+bowler.play()
